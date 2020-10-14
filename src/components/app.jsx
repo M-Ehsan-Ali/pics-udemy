61b9e3485dad9590edcc0516650fab1fdf3c1e1f
Ehsan Ali <71366898+mrehsan51@users.noreply.github.com>
2020-10-14 13:48:17 +0500

    Update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './searchBar';
 
-const App = () => {
+class App extends Component{
+    onSearchSubmit(term){
+        console.log(term);
+    }
+    render() {
     return ( <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit}/>
     </div> );
+}
 }
  
 export default App;
